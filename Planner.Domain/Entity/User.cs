de762e35348a3d9ca943632c4ef68e03fe643723
sl-aj <superslaj@gmail.com>
2024-09-04 23:18:10 +0200

    Add project files.

---
+++ Planner.Domain/Entity/User.cs
@@ -0,0 +1,28 @@
+﻿using Planner.Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Domain.Entity
+{
+    public class User : IEntityId<long>, IAuditable
+    {
+        public long Id { get; set; }
+
+        public string Login { get; set; }
+
+        public string Password { get; set; }
+
+        public List<Report> Reports { get; set; }
+
+        public DateTime CreatedAt { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public long UpdatedBy { get; set; }
+    }
+}
