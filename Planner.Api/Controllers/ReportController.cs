0c0d6ef325222315447389ed1483f627cdb1392e
sl-aj <superslaj@gmail.com>
2024-09-04 23:18:10 +0200

    Add project files.

---
+++ Planner.Api/Controllers/ReportController.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Planner.Api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class ReportController : ControllerBase
+    {
+    }
+}
---
+++ Planner.Domain/Interfaces/IAuditable.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Planner.Domain.Interfaces
+{
+    public interface IAuditable
+    {
+        public DateTime CreatedAt { get; set; }
+
+        public long CreatedBy { get; set; }
+
+        public DateTime UpdatedAt { get; set; }
+
+        public long UpdatedBy { get; set; }
+    }
+}
